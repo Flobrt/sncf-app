657637abcaddd4016695be3ddd969e55cd740f1f
flobert <berthelot_florian@yahoo.fr>
2026-05-13 15:12:57 +0200

    init

---
+++ app/connection.py
@@ -0,0 +1,22 @@
+import os
+from databricks import sql
+from dotenv import load_dotenv
+import pandas as pd
+
+load_dotenv()
+
+def get_connection():
+    return sql.connect(
+        server_hostname = os.getenv("DATABRICKS_HOST"),
+        http_path = os.getenv("DATABRICKS_HTTP"),
+        access_token = os.getenv("DATABRICKS_TOKEN")
+    )
+
+
+def run_query(query: str):
+    with get_connection() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(query)
+            columns = [desc[0] for desc in cursor.description]
+            rows = cursor.fetchall()
+            return pd.DataFrame(rows, columns=columns)
---
+++ app/main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+from connection import run_query
+
+st.set_page_config(page_title="SNCF Dashboard", layout="wide")
+st.title("🚄 Dashboard SNCF")
+
+
+
