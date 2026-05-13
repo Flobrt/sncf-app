b3b41e9c0760b881a8276e804c4cfb5a2a9d6d33
flobert <berthelot_florian@yahoo.fr>
2026-05-14 00:14:23 +0200

    v2-01

--- app/connection.py
+++ app/connection.py
@@ -1,15 +1,25 @@
 import os
-import pandas as pd
 from databricks import sql
 from dotenv import load_dotenv
+import pandas as pd
 
 load_dotenv()
 
 def get_connection():
+    try:
+        import databricks.sdk.runtime as sdk
+        dbutils = sdk.dbutils
+        server_hostname = dbutils.secrets.get(scope="my_app_scope", key="DATABRICKS_HOST")
+        http_path       = dbutils.secrets.get(scope="my_app_scope", key="DATABRICKS_HTTP")
+        access_token    = dbutils.secrets.get(scope="my_app_scope", key="DATABRICKS_TOKEN")
+    except Exception:
+        server_hostname = os.getenv("DATABRICKS_HOST")
+        http_path       = os.getenv("DATABRICKS_HTTP")
+        access_token    = os.getenv("DATABRICKS_TOKEN")
     return sql.connect(
-        server_hostname=os.getenv("DATABRICKS_HOST"),
-        http_path=os.getenv("DATABRICKS_HTTP"),
-        access_token=os.getenv("DATABRICKS_TOKEN")
+        server_hostname=server_hostname,
+        http_path=http_path,
+        access_token=access_token
     )
 
 def run_query(query: str):
