ff8771bb8547223ffd05b813ab140eb80b13b075
flobert <berthelot_florian@yahoo.fr>
2026-05-14 22:29:20 +0200

    v2-02

--- app/Trajet_SNCF.py
+++ app/Trajet_SNCF.py
@@ -39,8 +39,8 @@ def load_destinations(date, gare_depart, duree_max_h, train_types, heure_min, he
             FROM transport.gold.gtfs_gold
             WHERE date = '{date}'
             AND stop_name = '{gare_depart}'
-            CAST(departure_time AS STRING) >= '{heure_min}'
-            CAST(departure_time AS STRING) <= '{heure_max}'
+            AND CAST(departure_time AS STRING) >= '{heure_min}'
+            AND CAST(departure_time AS STRING) <= '{heure_max}'
             {train_filter}
         )
         SELECT DISTINCT
